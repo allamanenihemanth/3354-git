59e3c00e500fae76de537735b1d882cdf706288c
allamanenihemanth <71850644+allamanenihemanth@users.noreply.github.com>
2020-09-24 21:16:05 -0500

    Update Main.java

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,14 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide at least two numbers to add");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        int sum=Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+        for(int i=2;i<args.length;i++)
+            sum+=Integer.parseInt(args[i]);
+        return sum;
     }
 }
