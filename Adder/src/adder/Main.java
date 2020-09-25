b3bc1c412080a181d7618af5c7a67b511328c213
allamanenihemanth <71850644+allamanenihemanth@users.noreply.github.com>
2020-09-24 22:24:47 -0500

    Update Main.java

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -13,10 +13,15 @@ public class Main {
         
     }
 
-    private static int addArguments(String[] args) {
-          if (args[1] == "-") {
+      private static int addArguments(String[] args) {
+<<<<<<< HEAD
+        int sum=Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+        for(int i=2;i<args.length;i++)
+            sum+=Integer.parseInt(args[i]);
+        return sum;
             return -1*(Integer.valueOf(args[1]) + Integer.valueOf(args[2]))
         } else
         return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
     }
+>>>>>>> experimental-changes
 }
